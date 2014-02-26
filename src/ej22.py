ae2213e52dc26738263eef6a0b97777ee61baa43
Alba de León Hernández <alu0100831016@ull.edu.es>
2014-02-26 13:07:49 +0000

    ej22

---
+++ src/ej22.py
@@ -0,0 +1,11 @@
+#! encoding: UTF-8
+#! /usr/bin/python
+
+a = float(raw_input('Valor de a: '))
+b = float(raw_input('Valor de b: '))
+
+if a != 0:
+  x = -b/a
+  print 'Solucion: ', x
+if a = 0:
+print 'La ecuación no tiene solucion.'
